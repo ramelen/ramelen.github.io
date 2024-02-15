7c6fc60ebbfa0e690bfe8e26a8aab617947e0f7d
ramelen <ramelen@proton.me>
2024-02-14 23:15:41 -0800

    cleaning

--- motd.js
+++ motd.js
@@ -1,34 +1,35 @@
 const motds = [
-    "Ugh, I've gotta come with with 31 of these things.",
-    "Oh, crikey...!",
-    "Babies eat plastic all the time! It's probably fine.",
-    "No mercy >:3",
-    "This text doesn't add much value, but it looks cool.",
+    "ugh I've gotta come with with 31 of these things",
+    "crikey",
+    "babies eat plastic all the time it's probably fine",
+    "no mercy",
+    "this text doesn't add much value but it looks cool",
     "can you tell im a minimalist",
-    "I can't wait to watch my grades tank when Hades 2 comes out.",
-    "Bias is inevitable in the production of knowledge.",
-    "100% free of contradictions!",
-    "Hexagons are the Bestagons! :)",
+    "what do you get if you multiply six by nine?",
+    "death to chronos",
+    "completely free of contradictions!",
+    "hexagons are the bestagons",
     "I once walked 16k just to get away from my homework.",
-    "Every night before I go to bed I dab on them haters.",
-    "I love talking to people; shoot me an email!",
+    "yeah just lemme do this one thing real quick",
+    "glory to the watcher",
     "jan pi toki pona o epiku a",
-    "Everything in this website is aligned on a terminal-like grid.",
-    "Dozenal is superior, but not if we have to switch.",
-    "Man, IB is HARD.",
+    "what the heck are clouds",
+    "suprisingly binary is actually the best base",
+    "why are there so many things that i want to do",
     "mwahahahahaHAHAHAHAHA!",
-    "\"You\" needs to stop eating pronouns, dammit!",
-    "Spaaaaaaaaaaaaaaaaaaaaaaaaaaaaace!",
-    "It is pitch dark. You're likely to be eaten by a grue.",
-    "If everyone is you, being mean makes no sense.",
-    "I'm in a choir but I'll never say which one.",
-    "Mostly Harmless.",
+    "some people say one contains zero if you look close enough",
+    "shave and a haircut: one crumb",
+    "it is pitch black. you are likely to be eaten by a grue.",
+    "its ineffable!",
+    "here we are in the future and its bright",
+    "mostly harmless.",
     "There aren't any secrets in this website. Yet.",
-    "Whatevs, man. :/",
-    "Hello from 2023!",
-    "No hablo español muy bien, pero lo estoy aprendiendo... :)",
-    "Don't confuse confidence and being an asshole.",
-    "All likelyhood states that nobody ever sees this but me.",
+    "whatevs, man",
+    "hello from 2023!",
+    "no hablo español muy bien, pero estoy aprendiendo...",
+    "don't confuse confidence and being an asshole.",
+    "all likelyhood states that nobody ever sees this but me.",
+    "you are always exactly where you need to be."
 ];
 
 var today = new Date();
@@ -36,7 +37,7 @@ var message;
 
 switch (today.getMonth()+1+"/"+today.getDate()) {
     case "1/1"  : message = "Welcome to " + today.getFullYear() + "!"; break;
-    case "2/29" : message = "There's about a 1 in 1461 chance you see this on a random day."; break;
+    case "2/29" : message = "There's a 1 in 1461 chance you see this on a random day."; break;
     case "3/14" : message = "Its pi day! But days aren't decimal so not really..."; break;
     case "4/1"  : message = "My pronouns are she/her."; break;
     case "4/16" : message = "Today is the anniversary of the day I came up with PFIM."; break;
